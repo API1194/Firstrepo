7eaf8ac1c0dbd86f80577f434d6ac0c28bce6327
API1194 <harsh.sharma1@apisero.com>
2021-06-03 00:20:53 +0530

    Commited Number: 15

---
+++ Copying.java
@@ -0,0 +1,11 @@
+
+public class Copying{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Copying from desktop to demoproject by jenkins batch cmd");
+
+	}
+
+}
